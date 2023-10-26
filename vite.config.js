338d2104f42b064af6c5453514fc8b354df7ba74
DeveloperStevenMarin <developerstevenmarin@gmail.com>
2023-10-25 21:51:21 -0500

    project init

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import react from '@vitejs/plugin-react'; // Importa el plugin de React
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    _global: {},
-  },
-  root: 'src',
-  build: {
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html']), react()], // Agrega el plugin de React
-});
+  plugins: [react()],
+})
