94ef1ae3b9ed99e17a32fe2f706df07181b13776
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-10-25 20:05:28 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import react from '@vitejs/plugin-react'; // Importa el plugin de React
 
 export default defineConfig({
   define: {
@@ -14,5 +15,5 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [injectHTML(), FullReload(['./src/**/**.html']), react()], // Agrega el plugin de React
 });
