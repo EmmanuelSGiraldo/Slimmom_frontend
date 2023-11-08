4b06f121299859fef3fbfd021e925907be3203dd
EmmanuelSGiraldo <esemmanuel2022@gmail.com>
2023-11-07 23:11:53 -0500

    active gh-pages

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import authSelectors from 'redux/auth/authSelector';
 
+
 export default function PublicRoute({
   children,
   restricted = false,
